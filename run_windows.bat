27450f0f09c42ae204c1f92a2a06220c48e3d57c
3ternal13 <46868314+3ternal13@users.noreply.github.com>
2019-01-21 15:03:57 -0200

    Add files via upload

---
+++ run_windows.bat
@@ -0,0 +1,15 @@
+:inicio
+@ECHO OFF
+@break off
+color 0B
+mode con:cols=100 lines=30
+title JMusicBot
+@echo JMusicBot
+@echo BOT Iniciado Com Sucesso
+@echo off
+java -jar JMusicBot-0.1.3-Windows.jar -nogui
+echo Reiniciando o BOT...
+echo.
+echo.
+echo.
+goto inicio
